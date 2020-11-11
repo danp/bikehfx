a1acc3320e3e3321e8f2ce9c74d33d5694ac902e
Dan Peterson <dpiddy@gmail.com>
2020-11-11 19:29:04 -0400

    cmd/bikehfx-tweet: refresh graph
    Update gonum.org/v1/plot, embed and use Arial, change up graph style.

--- cmd/bikehfx-tweet/graph.go
+++ cmd/bikehfx-tweet/graph.go
@@ -2,29 +2,48 @@ package main
 
 import (
 	"bytes"
+	_ "embed"
+	"image"
 	"image/color"
+	"image/draw"
+	"image/png"
 	"time"
 
 	"github.com/danp/bikehfx/ecocounter"
+	"github.com/golang/freetype/truetype"
 	"gonum.org/v1/plot"
 	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/plotutil"
 	"gonum.org/v1/plot/vg"
 )
 
 func makeHourlyGraph(day time.Time, counters []counter) ([]byte, error) {
+	//go:embed Arial.ttf
+	var arial []byte
+	fontTTF, err := truetype.Parse(arial)
+	if err != nil {
+		return nil, err
+	}
+	const fontName = "Arial"
+	vg.AddFont(fontName, fontTTF)
+	plot.DefaultFont = fontName
+	plotter.DefaultFont = fontName
+	plotutil.DefaultColors = plotutil.DarkColors
+
 	p, err := plot.New()
 	if err != nil {
 		return nil, err
 	}
-	p.Title.Text = "Hourly counts for " + day.Format("Mon Jan 2")
+	p.Title.Text = "Counts for " + day.Format("Mon Jan 2") + " by hour starting"
 	p.X.Tick.Marker = hourTicker(day)
-	p.X.Label.Text = "Hour"
 	p.Y.Min = 0
 	p.Y.Label.Text = "Count"
 	p.Legend.Top = true
-	p.Legend.Left = true
 
-	p.Add(plotter.NewGrid())
+	grid := plotter.NewGrid()
+	grid.Vertical.Color = color.Gray{175}
+	grid.Horizontal.Color = color.Gray{175}
+	p.Add(grid)
 
 	for i, c := range counters {
 		ds, err := c.querier.query(day, ecocounter.ResolutionHour)
@@ -42,9 +61,12 @@ func makeHourlyGraph(day time.Time, counters []counter) ([]byte, error) {
 				return nil, err
 			}
 
-			if d.Count > 0 {
-				any = true
+			// Skip adding 0s until the first hour with a count >0.
+			// This cuts down on 0s for the early hours of the day.
+			if d.Count == 0 && !any {
+				continue
 			}
+			any = true
 
 			var xy struct {
 				X, Y float64
@@ -56,22 +78,23 @@ func makeHourlyGraph(day time.Time, counters []counter) ([]byte, error) {
 		}
 
 		if !any {
-			continue // no data for this period, do not include
+			continue // no data for this day, do not include
 		}
 
-		ln, _, err := plotter.NewLinePoints(data)
+		ln, err := plotter.NewLine(data)
 		if err != nil {
 			return nil, err
 		}
 
-		ln.Color = lineColor(i)
-		ln.Dashes = strokeDashArray(i)
+		ln.LineStyle.Color = plotutil.Color(i)
+		ln.LineStyle.Dashes = plotutil.Dashes(i)
+		ln.LineStyle.Width = vg.Points(2)
 
 		p.Add(ln)
 		p.Legend.Add(c.name, ln)
 	}
 
-	wt, err := p.WriterTo(28*vg.Centimeter, 10*vg.Centimeter, "png")
+	wt, err := p.WriterTo(20*vg.Centimeter, 10*vg.Centimeter, "png")
 	if err != nil {
 		return nil, err
 	}
@@ -81,26 +104,24 @@ func makeHourlyGraph(day time.Time, counters []counter) ([]byte, error) {
 		return nil, err
 	}
 
-	return b.Bytes(), nil
-}
-
-var colors = []color.Color{
-	color.RGBA{R: 0, G: 116, B: 217, A: 255}, // blue
-	color.RGBA{R: 0, G: 217, B: 210, A: 255}, // cyan
-	color.RGBA{R: 0, G: 217, B: 101, A: 255}, // green
-	color.RGBA{R: 217, G: 0, B: 116, A: 255}, // red
-	color.RGBA{R: 217, G: 101, B: 0, A: 255}, // orange
-}
-
-func lineColor(index int) color.Color {
-	return colors[index%len(colors)]
-}
-
-func strokeDashArray(index int) []vg.Length {
-	if index == 0 {
-		return nil
+	img, _, err := image.Decode(&b)
+	if err != nil {
+		return nil, err
 	}
-	return []vg.Length{vg.Length(index * 3), vg.Length(index * 2)}
+
+	bnds := img.Bounds()
+	const padding = 20
+	outRect := image.Rect(bnds.Min.X-padding, bnds.Min.Y-padding, bnds.Max.X+padding, bnds.Max.Y+padding)
+	out := image.NewRGBA(outRect)
+	draw.Draw(out, out.Bounds(), &image.Uniform{color.RGBA{255, 255, 255, 255}}, image.ZP, draw.Src)
+	draw.Draw(out, img.Bounds(), img, outRect.Min.Add(image.Pt(padding, padding)), draw.Over)
+
+	b.Reset()
+	if err := png.Encode(&b, out); err != nil {
+		return nil, err
+	}
+
+	return b.Bytes(), nil
 }
 
 type hourTicker time.Time
@@ -115,7 +136,7 @@ func (h hourTicker) Ticks(min, max float64) []plot.Tick {
 		if i%2 == 0 {
 			var tt time.Time
 			tt = tt.Add(time.Duration(i) * time.Hour)
-			t.Label = tt.Format("3 PM")
+			t.Label = tt.Format("3PM")
 		}
 		ts = append(ts, t)
 	}
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/dghubble/go-twitter v0.0.0-20170910035229-c4115fa44a92
 	github.com/dghubble/oauth1 v0.4.0
 	github.com/dghubble/sling v1.1.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
 	github.com/google/go-querystring v0.0.0-20170111101155-53e6ce116135 // indirect
 	github.com/joeshaw/envdecode v0.0.0-20180312135643-c9e015854467
 	github.com/stretchr/testify v1.5.1 // indirect
