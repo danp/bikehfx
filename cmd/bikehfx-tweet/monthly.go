f928daa5e7dd945853999a0e408d618d9e35f799
Dan Peterson <danp@danp.net>
2023-11-18 15:18:06 -0400

    monthly,yearly: make more like weekly

--- cmd/bikehfx-tweet/monthly.go
+++ cmd/bikehfx-tweet/monthly.go
@@ -3,20 +3,23 @@ package main
 import (
 	"context"
 	"flag"
+	"slices"
+	"sort"
+	"strings"
 	"time"
 
 	"github.com/graxinc/errutil"
 	"github.com/peterbourgon/ff/v3/ffcli"
+	"golang.org/x/exp/maps"
 	"golang.org/x/text/language"
 	"golang.org/x/text/message"
 )
 
 func newMonthlyCmd(rootConfig *rootConfig) *ffcli.Command {
 	var (
-		fs       = flag.NewFlagSet("bikehfx-tweet monthly", flag.ExitOnError)
-		month    = fs.String("month", time.Now().AddDate(0, -1, 0).Format("200601"), "month to tweet for, in YYYYMM form")
-		minMonth = fs.String("min-month", "", "minimum month to consider when graphing, otherwise looks back 8 months")
-		months   commaSeparatedString
+		fs     = flag.NewFlagSet("bikehfx-tweet monthly", flag.ExitOnError)
+		month  = fs.String("month", time.Now().AddDate(0, -1, 0).Format("200601"), "month to tweet for, in YYYYMM form")
+		months commaSeparatedString
 	)
 	fs.Var(&months, "months", "comma-separated months to tweet, in YYYYMM form, preferred over month")
 
@@ -31,206 +34,271 @@ func newMonthlyCmd(rootConfig *rootConfig) *ffcli.Command {
 				months = []string{*month}
 			}
 
-			var mm time.Time
-			if *minMonth != "" {
-				mmm, err := time.Parse("200601", *minMonth)
-				if err != nil {
-					return errutil.With(err)
-				}
-				mm = mmm
-			}
-
-			return monthlyExec(ctx, months, mm, rootConfig.ccd, rootConfig.trq, rootConfig.rc, rootConfig.twt)
+			return monthlyExec(ctx, months, rootConfig.ccd, rootConfig.trq, rootConfig.rc, rootConfig.twt)
 		},
 	}
 }
 
-func monthlyExec(ctx context.Context, months []string, minMonth time.Time, ccd cyclingCounterDirectory, trq timeRangeQuerier, rc recordsChecker, twt tweetThread) error {
+func monthlyExec(ctx context.Context, months []string, ccd cyclingCounterDirectory, trq timeRangeQuerier, rc recordsChecker, twt tweetThread) error {
 	loc, err := time.LoadLocation("America/Halifax")
 	if err != nil {
 		return errutil.With(err)
 	}
 
-	if !minMonth.IsZero() {
-		minMonth = time.Date(minMonth.Year(), minMonth.Month(), 1, 0, 0, 0, 0, loc)
-	}
+	trq2 := counterbaseTimeRangeQuerierV2{ccd, trq}
 
-	tweets := make([]tweet, 0, len(months))
-
+	var tweets []tweet
 	for _, month := range months {
-		montht, err := time.Parse("200601", month)
+		montht, err := time.ParseInLocation("200601", month, loc)
 		if err != nil {
 			return errutil.With(err)
 		}
 
-		monthRange := newTimeRangeDate(time.Date(montht.Year(), montht.Month(), 1, 0, 0, 0, 0, loc), 0, 1, 0)
-
-		monthRanges := []timeRange{monthRange}
-		for year := monthRange.begin.Year() - 1; year >= 2017 && len(monthRanges) < 8; year-- {
-			diff := monthRange.begin.Year() - year
-			monthRanges = append(monthRanges, monthRange.addDate(-diff, 0, 0))
-		}
-
-		var monthsSeries [][]counterSeries
-		for _, mr := range monthRanges {
-			counters, err := ccd.counters(ctx, mr)
-			if err != nil {
-				return errutil.With(err)
-			}
-
-			monthSeries, err := trq.queryCounterSeries(ctx, counters, []timeRange{mr})
-			if err != nil {
-				return errutil.With(err)
-			}
-
-			monthsSeries = append(monthsSeries, monthSeries)
-		}
-
-		records, err := rc.check(ctx, monthRange.begin, monthsSeries[0], recordWidthMonth)
+		ts, err := monthPost(ctx, montht, trq2, rc)
 		if err != nil {
 			return errutil.With(err)
 		}
 
-		monthTweetText := tweetText(monthsSeries[0], records, func(p *message.Printer, sum string) string {
-			return p.Sprintf("Month review:\n\n%s #BikeHfx trips counted in %s", sum, monthRange.begin.Format("Jan"))
-		})
-
-		graphBegin := monthRange.begin.AddDate(0, -7, 0)
-		if graphBegin.Before(minMonth) {
-			graphBegin = minMonth
-		}
-		graphRange := newTimeRangeDate(graphBegin, 0, 8, 0)
-		graphMonths := graphRange.splitDate(0, 1, 0)
-
-		graphCountRange := graphRange
-		if graphCountRange.end.After(monthRange.end) {
-			graphCountRange.end = monthRange.end
-		}
-		graphCountMonths := graphCountRange.splitDate(0, 1, 0)
-
-		graphCounters, err := ccd.counters(ctx, graphCountRange)
-		if err != nil {
-			return errutil.With(err)
-		}
-
-		graphCountSeries, err := trq.queryCounterSeries(ctx, graphCounters, graphCountMonths)
-		if err != nil {
-			return errutil.With(err)
-		}
-
-		monthCounts := make(map[time.Time]int)
-		for _, cs := range graphCountSeries {
-			for _, s := range cs.series {
-				monthCounts[s.tr.begin] += s.val
-			}
-		}
-
-		var graphTRVs []timeRangeValue
-		for _, gm := range graphMonths {
-			graphTRVs = append(graphTRVs, timeRangeValue{tr: gm, val: monthCounts[gm.begin]})
-		}
-
-		var graphCountTRVs []timeRangeValue
-		for _, gm := range graphCountMonths {
-			graphCountTRVs = append(graphCountTRVs, timeRangeValue{tr: gm, val: monthCounts[gm.begin]})
-		}
-
-		gr, err := timeRangeBarGraph(graphTRVs, "Total count by month", func(tr timeRange) string { return tr.begin.Format("Jan") })
-		if err != nil {
-			return errutil.With(err)
-		}
-
-		atg := altTextGenerator{
-			headlinePrinter: func(p *message.Printer, len int) string {
-				return p.Sprintf("Bar chart of counted cycling trips by month for last %d months.", len)
-			},
-			changePrinter: func(p *message.Printer, cur int, pctChange int) string {
-				if pctChange == 0 {
-					return p.Sprintf("The most recent month's count of %d is about the same as the previous month.", cur)
-				}
-
-				var moreOrFewer string
-				if pctChange > 0 {
-					moreOrFewer = "more"
-				} else {
-					moreOrFewer = "fewer"
-					pctChange *= -1
-				}
-				return p.Sprintf("The most recent month had %d trips counted, %d%% %s than the previous month.", cur, pctChange, moreOrFewer)
-			},
-		}
-
-		altText, err := atg.text(graphCountTRVs)
-		if err != nil {
-			return errutil.With(err)
-		}
-
-		tweets = append(tweets, tweet{
-			text: monthTweetText,
-			media: []tweetMedia{
-				{b: gr, altText: altText},
-			},
-		})
-
-		var graph2TRVs []timeRangeValue
-		for i, wr := range monthRanges {
-			ws := monthsSeries[i]
-			var sum int
-			for _, cs := range ws {
-				for _, s := range cs.series {
-					sum += s.val
-				}
-			}
-			graph2TRVs = append(graph2TRVs, timeRangeValue{tr: wr, val: sum})
-		}
-
-		prevMonthsTweetPrinter := message.NewPrinter(language.English)
-		prevMonthsTweetText := prevMonthsTweetPrinter.Sprintf("Previous year counts for %s:\n\n", monthRange.begin.Format("Jan"))
-		for _, trv := range graph2TRVs {
-			prevMonthsTweetText += prevMonthsTweetPrinter.Sprintf("%s: %d\n", trv.tr.end.Format("2006"), trv.val)
-		}
-
-		reverse(graph2TRVs)
-		gr2, err := timeRangeBarGraph(graph2TRVs, prevMonthsTweetPrinter.Sprintf("Total count for %s by year", monthRange.begin.Format("Jan")), func(tr timeRange) string { return tr.end.Format("2006") })
-		if err != nil {
-			return errutil.With(err)
-		}
-
-		atg2 := altTextGenerator{
-			headlinePrinter: func(p *message.Printer, len int) string {
-				return p.Sprintf("Bar chart of counted cycling trips for %s over last %d years.", monthRange.begin.Format("Jan"), len)
-			},
-			changePrinter: func(p *message.Printer, cur int, pctChange int) string {
-				if pctChange == 0 {
-					return p.Sprintf("The most recent years's count of %d is about the same as the previous year.", cur)
-				}
-
-				var moreOrFewer string
-				if pctChange > 0 {
-					moreOrFewer = "more"
-				} else {
-					moreOrFewer = "fewer"
-					pctChange *= -1
-				}
-				return p.Sprintf("The most recent year had %d trips counted, %d%% %s than the previous year.", cur, pctChange, moreOrFewer)
-			},
-		}
-
-		altText2, err := atg2.text(graph2TRVs)
-		if err != nil {
-			return errutil.With(err)
-		}
-
-		tweets = append(tweets, tweet{
-			text: prevMonthsTweetText,
-			media: []tweetMedia{
-				{b: gr2, altText: altText2},
-			},
-		})
+		tweets = append(tweets, ts...)
 	}
 
 	if _, err := twt.tweetThread(ctx, tweets); err != nil {
 		return errutil.With(err)
 	}
 	return nil
+}
+
+func monthPost(ctx context.Context, montht time.Time, trq counterbaseTimeRangeQuerierV2, rc recordsChecker) ([]tweet, error) {
+	var tweets []tweet
+
+	monthRange := newTimeRangeDate(time.Date(montht.Year(), montht.Month(), 1, 0, 0, 0, 0, montht.Location()), 0, 1, 0)
+
+	monthRanges := []timeRange{monthRange}
+	for year := monthRange.begin.Year() - 1; year >= 2017 && len(monthRanges) < 8; year-- {
+		diff := monthRange.begin.Year() - year
+		pw := monthRange.addDate(-diff, 0, 0)
+		monthRanges = append(monthRanges, pw)
+	}
+
+	var monthsSeries [][]counterSeriesV2
+	for _, wr := range monthRanges {
+		monthSeries, err := trq.query(ctx, wr)
+		if err != nil {
+			return nil, errutil.With(err)
+		}
+
+		monthsSeries = append(monthsSeries, monthSeries)
+	}
+
+	var cs1 []counterSeries
+	for _, c := range monthsSeries[0] {
+		if len(c.series) == 0 {
+			continue
+		}
+		cs1 = append(cs1, counterSeries{
+			counter: c.counter,
+			series:  c.series,
+		})
+	}
+	records, err := rc.check(ctx, monthRange.begin, cs1, recordWidthMonth)
+	if err != nil {
+		return nil, errutil.With(err)
+	}
+
+	monthTweetText := monthPostText(monthRange, monthsSeries[0], records)
+
+	graphBegin := monthRange.begin.AddDate(0, -7, 0)
+	graphRange := newTimeRangeDate(graphBegin, 0, 8, 0)
+	graphMonths := graphRange.splitDate(0, 1, 0)
+
+	graphCountSeries, err := trq.query(ctx, graphMonths...)
+	if err != nil {
+		return nil, errutil.With(err)
+	}
+
+	monthCounts := make(map[time.Time]int)
+	for _, cs := range graphCountSeries {
+		for _, s := range cs.series {
+			monthCounts[s.tr.begin] += s.val
+		}
+	}
+
+	var graphTRVs []timeRangeValue
+	for _, gm := range graphMonths {
+		graphTRVs = append(graphTRVs, timeRangeValue{tr: gm, val: monthCounts[gm.begin]})
+	}
+
+	gr, err := timeRangeBarGraph(graphTRVs, "Total count by month", func(tr timeRange) string { return tr.begin.Format("Jan") })
+	if err != nil {
+		return nil, errutil.With(err)
+	}
+
+	atg := altTextGenerator{
+		headlinePrinter: func(p *message.Printer, len int) string {
+			return p.Sprintf("Bar chart of counted cycling trips by month for last %d months.", len)
+		},
+		changePrinter: func(p *message.Printer, cur int, pctChange int) string {
+			if pctChange == 0 {
+				return p.Sprintf("The most recent month's count of %d is about the same as the previous month.", cur)
+			}
+
+			var moreOrFewer string
+			if pctChange > 0 {
+				moreOrFewer = "more"
+			} else {
+				moreOrFewer = "fewer"
+				pctChange *= -1
+			}
+			return p.Sprintf("The most recent month had %d trips counted, %d%% %s than the previous month.", cur, pctChange, moreOrFewer)
+		},
+	}
+
+	altText, err := atg.text(graphTRVs)
+	if err != nil {
+		return nil, errutil.With(err)
+	}
+
+	tweets = append(tweets, tweet{
+		text: monthTweetText,
+		media: []tweetMedia{
+			{b: gr, altText: altText},
+		},
+	})
+
+	var graph2TRVs []timeRangeValue
+	for i, wr := range monthRanges {
+		ws := monthsSeries[i]
+		var sum int
+		for _, cs := range ws {
+			for _, s := range cs.series {
+				sum += s.val
+			}
+		}
+		graph2TRVs = append(graph2TRVs, timeRangeValue{tr: wr, val: sum})
+	}
+
+	prevMonthsTweetPrinter := message.NewPrinter(language.English)
+	prevMonthsTweetText := prevMonthsTweetPrinter.Sprintf("Previous year counts for %v:\n\n", monthRange.begin.Format("Jan"))
+	for _, trv := range graph2TRVs {
+		prevMonthsTweetText += prevMonthsTweetPrinter.Sprintf("%v: %v\n", trv.tr.begin.Format("2006"), trv.val)
+	}
+
+	reverse(graph2TRVs)
+	gr2, err := timeRangeBarGraph(graph2TRVs, prevMonthsTweetPrinter.Sprintf("Total count for month %v by year", monthRange.begin.Format("Jan")), func(tr timeRange) string { return tr.begin.Format("2006") })
+	if err != nil {
+		return nil, errutil.With(err)
+	}
+
+	atg2 := altTextGenerator{
+		headlinePrinter: func(p *message.Printer, len int) string {
+			return p.Sprintf("Bar chart of counted cycling trips for month %v over last %d years.", monthRange.begin.Format("Jan"), len)
+		},
+		changePrinter: func(p *message.Printer, cur int, pctChange int) string {
+			if pctChange == 0 {
+				return p.Sprintf("The most recent years's count of %d is about the same as the previous year.", cur)
+			}
+
+			var moreOrFewer string
+			if pctChange > 0 {
+				moreOrFewer = "more"
+			} else {
+				moreOrFewer = "fewer"
+				pctChange *= -1
+			}
+			return p.Sprintf("The most recent year had %d trips counted, %d%% %s than the previous year.", cur, pctChange, moreOrFewer)
+		},
+	}
+
+	altText2, err := atg2.text(graph2TRVs)
+	if err != nil {
+		return nil, errutil.With(err)
+	}
+
+	tweets = append(tweets, tweet{
+		text: prevMonthsTweetText,
+		media: []tweetMedia{
+			{b: gr2, altText: altText2},
+		},
+	})
+
+	return tweets, nil
+}
+
+func monthPostText(monthRange timeRange, cs []counterSeriesV2, records map[string]recordKind) string {
+	var out strings.Builder
+
+	p := message.NewPrinter(language.English)
+
+	var sum int
+	presentIncompleteIndices := make(map[int]struct{})
+	var presentIndices, missingIndices []int
+	end := monthRange.end.AddDate(0, 0, -1)
+	for i, c := range cs {
+		for _, v := range c.series {
+			sum += v.val
+		}
+
+		if c.last.Before(monthRange.begin) || c.lastNonZero.Before(monthRange.begin) {
+			missingIndices = append(missingIndices, i)
+			continue
+		}
+
+		presentIndices = append(presentIndices, i)
+		if c.last.Before(end) || c.lastNonZero.Before(end) {
+			presentIncompleteIndices[i] = struct{}{}
+		}
+	}
+
+	p.Fprintf(&out, "Month review:\n\n%v%v #BikeHfx trips counted in %v\n\n", sum, recordSymbol(records["sum"]), monthRange.begin.Format("Jan"))
+
+	sort.Slice(presentIndices, func(i, j int) bool {
+		i, j = presentIndices[i], presentIndices[j]
+		return cs[i].counter.Name < cs[j].counter.Name
+	})
+	for _, i := range presentIndices {
+		c := cs[i]
+		v := c.series[len(c.series)-1].val
+		p.Fprintf(&out, "%v%v %v", v, recordSymbol(records[c.counter.ID]), c.counter.Name)
+		if _, ok := presentIncompleteIndices[i]; ok {
+			last := c.last
+			if !c.lastNonZero.IsZero() {
+				last = c.lastNonZero
+			}
+			p.Fprintf(&out, " (last %v)", last.Format("Jan 2"))
+		}
+		p.Fprintln(&out)
+	}
+
+	recordKinds := make(map[recordKind]struct{})
+	for _, k := range records {
+		recordKinds[k] = struct{}{}
+	}
+	if len(recordKinds) > 0 {
+		keys := maps.Keys(recordKinds)
+		slices.Sort(keys)
+		p.Fprintln(&out)
+		for _, k := range keys {
+			p.Fprintln(&out, recordNote(k))
+		}
+	}
+
+	if len(missingIndices) > 0 {
+		sort.Slice(missingIndices, func(i, j int) bool {
+			i, j = missingIndices[i], missingIndices[j]
+			return cs[i].counter.Name < cs[j].counter.Name
+		})
+
+		p.Fprintln(&out)
+		p.Fprintln(&out, "Missing (last):")
+		for _, i := range missingIndices {
+			c := cs[i]
+			last := c.last
+			if !c.lastNonZero.IsZero() {
+				last = c.lastNonZero
+			}
+			p.Fprintf(&out, "%v (%v)\n", c.counter.Name, last.Format("Jan 2"))
+		}
+	}
+
+	return strings.TrimSpace(out.String())
 }
--- cmd/bikehfx-tweet/yearly.go
+++ cmd/bikehfx-tweet/yearly.go
@@ -3,19 +3,23 @@ package main
 import (
 	"context"
 	"flag"
+	"slices"
+	"sort"
+	"strings"
 	"time"
 
 	"github.com/graxinc/errutil"
 	"github.com/peterbourgon/ff/v3/ffcli"
+	"golang.org/x/exp/maps"
+	"golang.org/x/text/language"
 	"golang.org/x/text/message"
 )
 
 func newYearlyCmd(rootConfig *rootConfig) *ffcli.Command {
 	var (
-		fs      = flag.NewFlagSet("bikehfx-tweet yearly", flag.ExitOnError)
-		year    = fs.String("year", time.Now().AddDate(-1, 0, 0).Format("2006"), "year to tweet for, in YYYY form")
-		minYear = fs.String("min-year", "", "minimum year to consider when graphing, otherwise looks back 8 years")
-		years   commaSeparatedString
+		fs    = flag.NewFlagSet("bikehfx-tweet yearly", flag.ExitOnError)
+		year  = fs.String("year", time.Now().AddDate(-1, 0, 0).Format("2006"), "year to tweet for, in YYYY form")
+		years commaSeparatedString
 	)
 	fs.Var(&years, "years", "comma-separated years to tweet, in YYYY form, preferred over year")
 
@@ -30,139 +34,271 @@ func newYearlyCmd(rootConfig *rootConfig) *ffcli.Command {
 				years = []string{*year}
 			}
 
-			var mm time.Time
-			if *minYear != "" {
-				mmm, err := time.Parse("2006", *minYear)
-				if err != nil {
-					return errutil.With(err)
-				}
-				mm = mmm
-			}
-
-			return yearlyExec(ctx, years, mm, rootConfig.ccd, rootConfig.trq, rootConfig.rc, rootConfig.twt)
+			return yearlyExec(ctx, years, rootConfig.ccd, rootConfig.trq, rootConfig.rc, rootConfig.twt)
 		},
 	}
 }
 
-func yearlyExec(ctx context.Context, years []string, minYear time.Time, ccd cyclingCounterDirectory, trq timeRangeQuerier, rc recordsChecker, twt tweetThread) error {
+func yearlyExec(ctx context.Context, years []string, ccd cyclingCounterDirectory, trq timeRangeQuerier, rc recordsChecker, twt tweetThread) error {
 	loc, err := time.LoadLocation("America/Halifax")
 	if err != nil {
 		return errutil.With(err)
 	}
 
-	if !minYear.IsZero() {
-		minYear = time.Date(minYear.Year(), 1, 1, 0, 0, 0, 0, loc)
-	}
+	trq2 := counterbaseTimeRangeQuerierV2{ccd, trq}
 
-	tweets := make([]tweet, 0, len(years))
-
+	var tweets []tweet
 	for _, year := range years {
-		yeart, err := time.Parse("2006", year)
+		yeart, err := time.ParseInLocation("2006", year, loc)
 		if err != nil {
 			return errutil.With(err)
 		}
 
-		yearRange := newTimeRangeDate(time.Date(yeart.Year(), 1, 1, 0, 0, 0, 0, loc), 1, 0, 0)
-
-		counters, err := ccd.counters(ctx, yearRange)
+		ts, err := yearPost(ctx, yeart, trq2, rc)
 		if err != nil {
 			return errutil.With(err)
 		}
 
-		yearSeries, err := trq.queryCounterSeries(ctx, counters, []timeRange{yearRange})
-		if err != nil {
-			return errutil.With(err)
-		}
-
-		records, err := rc.check(ctx, yearRange.begin, yearSeries, recordWidthYear)
-		if err != nil {
-			return errutil.With(err)
-		}
-
-		mt := tweetText(yearSeries, records, func(p *message.Printer, sum string) string {
-			return p.Sprintf("Year review:\n\n%s #BikeHfx trips counted in %s", sum, yearRange.begin.Format("2006"))
-		})
-
-		graphBegin := yearRange.begin.AddDate(-7, 0, 0)
-		if graphBegin.Before(minYear) {
-			graphBegin = minYear
-		}
-		graphRange := newTimeRangeDate(graphBegin, 8, 0, 0)
-		graphYears := graphRange.splitDate(1, 0, 0)
-
-		graphCountRange := graphRange
-		if graphCountRange.end.After(yearRange.end) {
-			graphCountRange.end = yearRange.end
-		}
-		graphCountYears := graphCountRange.splitDate(1, 0, 0)
-
-		graphCounters, err := ccd.counters(ctx, graphCountRange)
-		if err != nil {
-			return errutil.With(err)
-		}
-
-		graphCountSeries, err := trq.queryCounterSeries(ctx, graphCounters, graphCountYears)
-		if err != nil {
-			return errutil.With(err)
-		}
-
-		yearCounts := make(map[time.Time]int)
-		for _, cs := range graphCountSeries {
-			for _, s := range cs.series {
-				yearCounts[s.tr.begin] += s.val
-			}
-		}
-
-		var graphTRVs []timeRangeValue
-		for _, gm := range graphYears {
-			graphTRVs = append(graphTRVs, timeRangeValue{tr: gm, val: yearCounts[gm.begin]})
-		}
-
-		var graphCountTRVs []timeRangeValue
-		for _, gm := range graphCountYears {
-			graphCountTRVs = append(graphCountTRVs, timeRangeValue{tr: gm, val: yearCounts[gm.begin]})
-		}
-
-		gr, err := timeRangeBarGraph(graphTRVs, "Total count by year", func(tr timeRange) string { return tr.begin.Format("2006") })
-		if err != nil {
-			return errutil.With(err)
-		}
-
-		atg := altTextGenerator{
-			headlinePrinter: func(p *message.Printer, len int) string {
-				return p.Sprintf("Bar chart of counted cycling trips by year for last %d years.", len)
-			},
-			changePrinter: func(p *message.Printer, cur int, pctChange int) string {
-				if pctChange == 0 {
-					return p.Sprintf("The most recent year's count of %d is about the same as the previous year.", cur)
-				}
-
-				var moreOrFewer string
-				if pctChange > 0 {
-					moreOrFewer = "more"
-				} else {
-					moreOrFewer = "fewer"
-					pctChange *= -1
-				}
-				return p.Sprintf("The most recent year had %d trips counted, %d%% %s than the previous year.", cur, pctChange, moreOrFewer)
-			},
-		}
-
-		altText, err := atg.text(graphCountTRVs)
-		if err != nil {
-			return errutil.With(err)
-		}
-
-		tweets = append(tweets, tweet{
-			text: mt,
-			media: []tweetMedia{
-				{b: gr, altText: altText},
-			},
-		})
+		tweets = append(tweets, ts...)
 	}
 
 	if _, err := twt.tweetThread(ctx, tweets); err != nil {
 		return errutil.With(err)
 	}
 	return nil
+}
+
+func yearPost(ctx context.Context, yeart time.Time, trq counterbaseTimeRangeQuerierV2, rc recordsChecker) ([]tweet, error) {
+	var tweets []tweet
+
+	yearRange := newTimeRangeDate(time.Date(yeart.Year(), 1, 1, 0, 0, 0, 0, yeart.Location()), 1, 0, 0)
+
+	yearRanges := []timeRange{yearRange}
+	for year := yearRange.begin.Year() - 1; year >= 2017 && len(yearRanges) < 8; year-- {
+		diff := yearRange.begin.Year() - year
+		pw := yearRange.addDate(-diff, 0, 0)
+		yearRanges = append(yearRanges, pw)
+	}
+
+	var yearsSeries [][]counterSeriesV2
+	for _, wr := range yearRanges {
+		yearSeries, err := trq.query(ctx, wr)
+		if err != nil {
+			return nil, errutil.With(err)
+		}
+
+		yearsSeries = append(yearsSeries, yearSeries)
+	}
+
+	var cs1 []counterSeries
+	for _, c := range yearsSeries[0] {
+		if len(c.series) == 0 {
+			continue
+		}
+		cs1 = append(cs1, counterSeries{
+			counter: c.counter,
+			series:  c.series,
+		})
+	}
+	records, err := rc.check(ctx, yearRange.begin, cs1, recordWidthYear)
+	if err != nil {
+		return nil, errutil.With(err)
+	}
+
+	yearTweetText := yearPostText(yearRange, yearsSeries[0], records)
+
+	graphBegin := yearRange.begin.AddDate(-7, 0, 0)
+	graphRange := newTimeRangeDate(graphBegin, 8, 0, 0)
+	graphYears := graphRange.splitDate(1, 0, 0)
+
+	graphCountSeries, err := trq.query(ctx, graphYears...)
+	if err != nil {
+		return nil, errutil.With(err)
+	}
+
+	yearCounts := make(map[time.Time]int)
+	for _, cs := range graphCountSeries {
+		for _, s := range cs.series {
+			yearCounts[s.tr.begin] += s.val
+		}
+	}
+
+	var graphTRVs []timeRangeValue
+	for _, gm := range graphYears {
+		graphTRVs = append(graphTRVs, timeRangeValue{tr: gm, val: yearCounts[gm.begin]})
+	}
+
+	gr, err := timeRangeBarGraph(graphTRVs, "Total count by year", func(tr timeRange) string { return tr.begin.Format("2006") })
+	if err != nil {
+		return nil, errutil.With(err)
+	}
+
+	atg := altTextGenerator{
+		headlinePrinter: func(p *message.Printer, len int) string {
+			return p.Sprintf("Bar chart of counted cycling trips by year for last %d years.", len)
+		},
+		changePrinter: func(p *message.Printer, cur int, pctChange int) string {
+			if pctChange == 0 {
+				return p.Sprintf("The most recent year's count of %d is about the same as the previous year.", cur)
+			}
+
+			var moreOrFewer string
+			if pctChange > 0 {
+				moreOrFewer = "more"
+			} else {
+				moreOrFewer = "fewer"
+				pctChange *= -1
+			}
+			return p.Sprintf("The most recent year had %d trips counted, %d%% %s than the previous year.", cur, pctChange, moreOrFewer)
+		},
+	}
+
+	altText, err := atg.text(graphTRVs)
+	if err != nil {
+		return nil, errutil.With(err)
+	}
+
+	tweets = append(tweets, tweet{
+		text: yearTweetText,
+		media: []tweetMedia{
+			{b: gr, altText: altText},
+		},
+	})
+
+	var graph2TRVs []timeRangeValue
+	for i, wr := range yearRanges {
+		ws := yearsSeries[i]
+		var sum int
+		for _, cs := range ws {
+			for _, s := range cs.series {
+				sum += s.val
+			}
+		}
+		graph2TRVs = append(graph2TRVs, timeRangeValue{tr: wr, val: sum})
+	}
+
+	prevYearsTweetPrinter := message.NewPrinter(language.English)
+	prevYearsTweetText := prevYearsTweetPrinter.Sprintf("Previous year counts:\n\n")
+	for _, trv := range graph2TRVs {
+		prevYearsTweetText += prevYearsTweetPrinter.Sprintf("%v: %v\n", trv.tr.begin.Format("2006"), trv.val)
+	}
+
+	reverse(graph2TRVs)
+	gr2, err := timeRangeBarGraph(graph2TRVs, prevYearsTweetPrinter.Sprintf("Total count by year"), func(tr timeRange) string { return tr.begin.Format("2006") })
+	if err != nil {
+		return nil, errutil.With(err)
+	}
+
+	atg2 := altTextGenerator{
+		headlinePrinter: func(p *message.Printer, len int) string {
+			return p.Sprintf("Bar chart of counted cycling trips over last %d years.", len)
+		},
+		changePrinter: func(p *message.Printer, cur int, pctChange int) string {
+			if pctChange == 0 {
+				return p.Sprintf("The most recent years's count of %d is about the same as the previous year.", cur)
+			}
+
+			var moreOrFewer string
+			if pctChange > 0 {
+				moreOrFewer = "more"
+			} else {
+				moreOrFewer = "fewer"
+				pctChange *= -1
+			}
+			return p.Sprintf("The most recent year had %d trips counted, %d%% %s than the previous year.", cur, pctChange, moreOrFewer)
+		},
+	}
+
+	altText2, err := atg2.text(graph2TRVs)
+	if err != nil {
+		return nil, errutil.With(err)
+	}
+
+	tweets = append(tweets, tweet{
+		text: prevYearsTweetText,
+		media: []tweetMedia{
+			{b: gr2, altText: altText2},
+		},
+	})
+
+	return tweets, nil
+}
+
+func yearPostText(yearRange timeRange, cs []counterSeriesV2, records map[string]recordKind) string {
+	var out strings.Builder
+
+	p := message.NewPrinter(language.English)
+
+	var sum int
+	presentIncompleteIndices := make(map[int]struct{})
+	var presentIndices, missingIndices []int
+	end := yearRange.end.AddDate(0, 0, -1)
+	for i, c := range cs {
+		for _, v := range c.series {
+			sum += v.val
+		}
+
+		if c.last.Before(yearRange.begin) || c.lastNonZero.Before(yearRange.begin) {
+			missingIndices = append(missingIndices, i)
+			continue
+		}
+
+		presentIndices = append(presentIndices, i)
+		if c.last.Before(end) || c.lastNonZero.Before(end) {
+			presentIncompleteIndices[i] = struct{}{}
+		}
+	}
+
+	p.Fprintf(&out, "Year review:\n\n%v%v #BikeHfx trips counted in %v\n\n", sum, recordSymbol(records["sum"]), yearRange.begin.Format("2006"))
+
+	sort.Slice(presentIndices, func(i, j int) bool {
+		i, j = presentIndices[i], presentIndices[j]
+		return cs[i].counter.Name < cs[j].counter.Name
+	})
+	for _, i := range presentIndices {
+		c := cs[i]
+		v := c.series[len(c.series)-1].val
+		p.Fprintf(&out, "%v%v %v", v, recordSymbol(records[c.counter.ID]), c.counter.Name)
+		if _, ok := presentIncompleteIndices[i]; ok {
+			last := c.last
+			if !c.lastNonZero.IsZero() {
+				last = c.lastNonZero
+			}
+			p.Fprintf(&out, " (last %v)", last.Format("Jan 2"))
+		}
+		p.Fprintln(&out)
+	}
+
+	recordKinds := make(map[recordKind]struct{})
+	for _, k := range records {
+		recordKinds[k] = struct{}{}
+	}
+	if len(recordKinds) > 0 {
+		keys := maps.Keys(recordKinds)
+		slices.Sort(keys)
+		p.Fprintln(&out)
+		for _, k := range keys {
+			p.Fprintln(&out, recordNote(k))
+		}
+	}
+
+	if len(missingIndices) > 0 {
+		sort.Slice(missingIndices, func(i, j int) bool {
+			i, j = missingIndices[i], missingIndices[j]
+			return cs[i].counter.Name < cs[j].counter.Name
+		})
+
+		p.Fprintln(&out)
+		p.Fprintln(&out, "Missing (last):")
+		for _, i := range missingIndices {
+			c := cs[i]
+			last := c.last
+			if !c.lastNonZero.IsZero() {
+				last = c.lastNonZero
+			}
+			p.Fprintf(&out, "%v (%v)\n", c.counter.Name, last.Format("Jan 2"))
+		}
+	}
+
+	return strings.TrimSpace(out.String())
 }
